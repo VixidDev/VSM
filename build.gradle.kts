186756be3a2b82b31109b5489e857aba3faa941d
VixidDev <52578495+VixidDev@users.noreply.github.com>
2025-09-08 17:30:42 +0100

    Update maven repo for loom

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     java
     id("gg.essential.loom") version "0.10.0.+"
     id("dev.architectury.architectury-pack200") version "0.1.3"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.4"
     kotlin("jvm") version "1.8.21"
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
         maven("https://maven.fabricmc.net")
         maven("https://maven.minecraftforge.net/")
         maven("https://repo.spongepowered.org/maven/")
-        maven("https://repo.sk1er.club/repository/maven-releases/")
+        maven("https://repo.essential.gg/repository/maven-public")
     }
     resolutionStrategy {
         eachPlugin {
@@ -21,6 +21,5 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
 }
-
 
 rootProject.name = "VSM"
